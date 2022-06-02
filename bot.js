3039cd4f733d5af846aa885c49b36ae3abadac66
ianyian <ianyian@gmail.com>
2022-06-02 08:56:33 +0800

    update

--- bot.js
+++ bot.js
@@ -1,9 +1,13 @@
 const Discord = require("discord.js");
 const settings = require("./settings.json");
+const WelcomeChannelId = settings.WelcomeChannelId;
+
+const generateImage = require("./generateImage");
+
 require("dotenv").config();
 
 const client = new Discord.Client({
-  intents: ["GUILDS", "GUILD_MESSAGES"],
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 });
 
 client.on("ready", () => {
@@ -14,6 +18,15 @@ client.on("messageCreate", (message) => {
   if (message.content == "hi") {
     message.reply("hello world");
   }
+});
+
+client.on("guildMemberAdd", async (member) => {
+  const img = await generateImage(member);
+
+  member.guild.channels.cache.get(WelcomeChannelId).send({
+    content: `<@${member.id} Welcome to the Server !`,
+    files: [img],
+  });
 });
 
 client.login(process.env.TOKEN);
